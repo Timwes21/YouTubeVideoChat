7b42715efd8753884b1a851d512ac8f6226649a7
Timothy Wesley <timothywesley@mac.mynetworksettings.com>
2025-07-29 04:14:43 -0400

    added the production route

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,6 +1,6 @@
-const developing = true;
+const developing = false;
 export const authBase = false?"http://localhost:8080": "https://youtubevideochat-551677223820.us-east1.run.app";
-const aiBase = developing? "http://0.0.0.0:8000" : "" 
+const aiBase = developing? "http://0.0.0.0:8000" : "https://youtubesummaraizerstore-551677223820.us-east1.run.app" 
 export const aiConvo = aiBase + "/talk-to-source";
 export const aiUpdate = aiBase + "/update-sources";
 export const aiInfo = aiBase + "/get-info";
